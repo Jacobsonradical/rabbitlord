8700046a09bb2acdd77119f2d409054d5f284135
Jacobsonradical <jacobsonradical@gmail.com>
2025-05-07 13:11:55 -0400

    1) Update pyproject.toml

---
+++ rabbits/rabbit_saver.py
@@ -0,0 +1,17 @@
+from atomicwrites import atomic_write
+import json
+import os
+
+
+def save_extend(output_fp, list_extend):
+    if os.path.exists(output_fp):
+        with open(output_fp, "rt") as f:
+            list_exist = json.load(f)
+    else:
+        list_exist = []
+    list_exist.extend(list_extend)
+    with atomic_write(output_fp, overwrite=True) as f:
+        json.dump(list_exist, f)
+
+
+
