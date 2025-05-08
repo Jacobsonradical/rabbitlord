1befaa872777a5fff237c28a3e819ebb5988caf0
Jacobsonradical <jacobsonradical@gmail.com>
2025-05-08 11:17:03 -0400

    Update rabbit_loader.py

---
+++ rabbits/rabbit_loader.py
@@ -0,0 +1,37 @@
+import polars as pl
+import os
+import zipfile
+
+
+def load_dataframe(fp):
+    _, ext = os.path.splitext(fp)
+    if ext not in {".zip", ".csv", ".tsv", ".json"}:
+        raise ValueError(f"Unsupported file type: {ext}.")
+    elif ext == ".zip":
+        with zipfile.ZipFile(fp, 'r') as z:
+            if len(z.namelist()) != 1:
+                raise ValueError("You have multiple files in the zip.")
+
+            inner_file = z.namelist()[0]
+            inner_ext = os.path.splitext(inner_file)[1].lower()
+            if inner_ext not in {".csv", ".tsv", ".json"}:
+                raise ValueError(f"Unsupported inner file type in zip: {ext}.")
+
+            with z.open(inner_file) as f:
+                if inner_ext == ".csv":
+                    df = pl.read_csv(f)
+                elif inner_ext == ".tsv":
+                    df = pl.read_csv(f, separator="\t")
+                else:
+                    df = pl.read_json(f)
+    elif ext == ".csv":
+        df = pl.read_csv(fp)
+    elif ext == ".tsv":
+        df = pl.read_csv(fp, separator="\t")
+    else:
+        df = pl.read_json(fp)
+    return df
+
+
+
+
