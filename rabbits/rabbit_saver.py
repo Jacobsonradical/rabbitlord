f4ed79122ac95f3d3ef5ffe52fad684e47a8289d
Jacobsonradical <jacobsonradical@gmail.com>
2025-05-21 08:35:13 -0400

    Update rabbit_saver.py

--- rabbits/rabbit_saver.py
+++ rabbits/rabbit_saver.py
@@ -14,4 +14,14 @@ def save_extend(output_fp, list_extend):
         json.dump(list_exist, f)
 
 
+def save_append(output_fp, to_append):
+    if os.path.exists(output_fp):
+        with open(output_fp, "rt") as f:
+            list_exist = json.load(f)
+    else:
+        list_exist = []
+    list_exist.append(to_append)
+    with atomic_write(output_fp, overwrite=True) as f:
+        json.dump(list_exist, f)
+
 
